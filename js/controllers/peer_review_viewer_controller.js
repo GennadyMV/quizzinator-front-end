ba32465cad486328aa1521ebe4e4b6b54fd7b6a2
Hattumies <ilmu90@hotmail.com>
2014-10-27 14:42:37 +0200

    korjattu typoja

--- js/controllers/peer_review_viewer_controller.js
+++ js/controllers/peer_review_viewer_controller.js
@@ -1,34 +1,39 @@
 QuizApp.controller('PeerReviewViewerController', ['$scope', 'API', function($scope, API){
-	$scope.number_of_reviews_on_list = 5;
+	$scope.count;
 	$scope.peer_reviews = [];
 	$scope.current_peer_reviews = [];
 	$scope.current_index;
 
-	$scope.get_peer_reviews = function() {
-		API.get_peer_reviews({
-			id: $scope.quiz_id,
-			success: function(reviews) {
-				$scope.peer_reviews = reviews;
-				$scope.current_index = 0;
-			}
-		})
+	$scope.init = function(item) {
+		$scope.count = item.count;
+		$scope.current_index = 0;
+		get_peer_reviews();
 	}
 
 	$scope.list_peer_reviews_forward = function() {
-		for(i=0;i < $scope.number_of_reviews_on_list;i++) {
+		for(i=0;i < $scope.count;i++) {
 			if($scope.current_index < $scope.peer_reviews) {
-			$scope.current_peer_reviews[i] = $scope.peer_reviews[$scope.current_index;
+			$scope.current_peer_reviews[i] = $scope.peer_reviews[$scope.current_index];
 			$scope.curent_index++; 
 			}
 		}
 	}
 
 	$scope.list_peer_reviews_backwards = function() {
-		for(i=0;i < $scope.number_of_reviews_on_list;i++) {
+		for(i=0;i < $scope.count;i++) {
 			if($scope.current_index >= 0) {
-			$scope.current_peer_reviews[i] = $scope.peer_reviews[$scope.current_index;
+			$scope.current_peer_reviews[i] = $scope.peer_reviews[$scope.current_index];
 			$scope.curent_index--; 
 			}
 		}
+	}
+
+	get_peer_reviews = function() {
+		API.get_peer_reviews({
+			id: $scope.quiz_id,
+			success: function(reviews) {
+				$scope.peer_reviews = reviews;
+			}
+		})
 	}
 }]);
