511146c965f1f7698792a0a0e2a5c2f6da6599d3
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-06-17 13:18:12 +0300

    usability improvements, preventing pasting

--- js/directives/code_typing.js
+++ js/directives/code_typing.js
@@ -8,15 +8,21 @@ QuizApp.directive('codetyping', function(){
 			scope.untyped = scope.item.code;
             scope.$watch('item.value', function(newVal) {
             	if (newVal === undefined) newVal = "";
-
-            	scope.untyped = scope.item.code.substring(newVal.length);
+            	var code = scope.item.code;
+            	scope.untyped = code.substring(newVal.length + 1);
             	var i = 0;
             	while (i < newVal.length) {
             		if (scope.item.code.charAt(i) !== newVal.charAt(i)) break;
             		i++;
             	}
-            	scope.correct = scope.item.code.substring(0, i);
-            	scope.wrong = scope.item.code.substring(i, newVal.length);
+            	scope.correct = code.substring(0, i);
+            	scope.wrong = code.substring(i, newVal.length);
+            	if(scope.wrong.length == 0){
+            		scope.current = code.charAt(i).replace(/\n/g,"⏎\n");
+            	} else {
+            		scope.current = "";
+            		scope.untyped = code.substring(newVal.length);
+            	}
             });
 		},
 		// You do not currently have the ability to access scope variables from the templateUrl function, since the template is requested before the scope is initialized.
