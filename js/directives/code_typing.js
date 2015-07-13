d51ac4ef8cf7986727d5db616ec0d653ea8868d5
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-07-13 16:04:29 +0300

    tab supported, but it doesn't highlight quickly enough

--- js/directives/code_typing.js
+++ js/directives/code_typing.js
@@ -5,14 +5,17 @@ QuizApp.directive('codetyping', function(){
 			item: '=ngModel'
 		},
 		link: function(scope, elem, attrs) {
-			scope.untyped = scope.item.code;
-            scope.$watch('item.value', function(newVal) {
+			$(elem).children('.code-typing').allowTabChar();
+                  scope.untyped = scope.item.code;
+                  scope.$watch('item.value', function(newVal) {
             	if (newVal === undefined) newVal = "";
-            	var code = scope.item.code;
+                  newVal = newVal.replace(/\t/g,"    ");
+                  scope.test = newVal;
+            	var code = scope.item.code.replace(/\t/g,"    ");
             	scope.untyped = code.substring(newVal.length + 1);
             	var i = 0;
             	while (i < newVal.length) {
-            		if (scope.item.code.charAt(i) !== newVal.charAt(i)) break;
+            		if (scope.item.code.replace(/\t/g,"    ").charAt(i) !== newVal.charAt(i)) break;
             		i++;
             	}
             	scope.correct = code.substring(0, i);
@@ -25,7 +28,53 @@ QuizApp.directive('codetyping', function(){
             	}
             });
 		},
-		// You do not currently have the ability to access scope variables from the templateUrl function, since the template is requested before the scope is initialized.
 		templateUrl: '../js/views/widgets/code_typing_template.html'
 	}
 });
+
+(function($) {
+    function pasteIntoInput(el, text) {
+        el.focus();
+        var val = el.value;
+        if (typeof el.selectionStart == "number") {
+            var selStart = el.selectionStart;
+            el.value = val.slice(0, selStart) + text + val.slice(el.selectionEnd);
+            el.selectionEnd = el.selectionStart = selStart + text.length;
+        } else if (typeof document.selection != "undefined") {
+            var textRange = document.selection.createRange();
+            textRange.text = text;
+            textRange.collapse(false);
+            textRange.select();
+        }
+    }
+
+    function allowTabChar(el) {
+        $(el).keydown(function(e) {
+            if (e.which == 9) {
+                pasteIntoInput(this, "    ");
+                return false;
+            }
+        });
+
+        // For Opera, which only allows suppression of keypress events, not keydown
+        $(el).keypress(function(e) {
+            if (e.which == 9) {
+                return false;
+            }
+        });
+    }
+
+    $.fn.allowTabChar = function() {
+        if (this.jquery) {
+            this.each(function() {
+                if (this.nodeType == 1) {
+                    var nodeName = this.nodeName.toLowerCase();
+                    if (nodeName == "textarea" || (nodeName == "input" && this.type == "text")) {
+                        allowTabChar(this);
+                    }
+                }
+            })
+        }
+        return this;
+    }
+})(jQuery);
